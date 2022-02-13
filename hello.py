64e63fea80357c6173617ab766a8ac94769b5190
meredithbartel <98117031+meredithbartel@users.noreply.github.com>
2022-02-13 11:48:28 -0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+ #Meredith Bartel  Bartel 2/7/22 code by "Automate the Boring Stuff with Python."
+print('Hello world!')
+print('What is your name?') #Ask for their name
+myName=input()
+print('It is good to meet you,' +myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?')   #ask for their age
+myAge=input()
+print('You will be '+str(int(myAge)+1) +' in a year.')
